4f88aed60be2c621b3416f35762fdde08d47494d
regalconcepts <naboafo18@gmail.com>
2024-01-13 19:36:01 +0000

    update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
-
-    for i in list_keys:
-        new_dir[i] *= 2
-
-        return (new_dir)
+    new_dic = {}
+    for i in a_dictionary:
+        new_dic[i] = a_dictionary[i] * 2
+    return new_dic
